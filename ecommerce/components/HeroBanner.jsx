133eaaafd92c643a939a1277a51239ad484fa1d6
MiguelA22 <mikkel.angel22@gmail.com>
2022-12-08 22:15:17 -0500

    Thirst

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Link from 'next/link';
+import {urlFor} from '../lib/client';
 
-const HeroBanner = () => {
+const HeroBanner = ({heroBanner}) => {
   return (
-    <div>HeroBanner</div>
+    <div>
+      <div className='hero-banner-container'>
+        <p className='beats-solo'>{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <img src={urlFor(heroBanner.image)} alt="banner" className='hero-banner-image'/>
+
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type='button'>{heroBanner.buttonText}</button>
+          </Link>
+          <div className='desc'>
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
