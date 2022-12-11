bbf3429f5a7bb049ccdfd2779bb579561e6df2cc
MiguelA22 <mikkel.angel22@gmail.com>
2022-12-11 16:11:41 -0500

    Cuarto

--- ecommerce/pages/Product/[slug].js
+++ ecommerce/pages/Product/[slug].js
@@ -1,9 +1,54 @@
 import React from 'react'
+import {client, urlFor} from '../../lib/client';
 
-const ProductsDetails  = () => {
+const ProductsDetails  = ({product,products}) => {
+  const {image,name,details, price} = product;
   return (
-    <div>ProductsDetails </div>
+    <div>
+      <div className='product-detail-container'>
+        <div>
+          <div className='image-container'>
+            <img src={urlFor(image && image[index])}/>
+          </div>
+        </div>
+      </div>
+    </div>
   )
+}
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+    slug {
+      current
+    }
+  }
+  `;
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: { 
+      slug: product.slug.current
+    }
+  }));
+
+  console.log(products)
+  return {
+    paths,
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps = async ({params:{slug}}) =>{
+  const query = `*[_type == "product" && slug.current = '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  return{
+    props:{products, product}
+  }
 }
 
 export default ProductsDetails 
