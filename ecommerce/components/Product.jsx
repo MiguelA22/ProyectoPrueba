849d690f79a8caa780b1aa5627feb09d9413cb95
MiguelA22 <mikkel.angel22@gmail.com>
2022-12-10 21:42:18 -0500

    Cuarto

--- ecommerce/components/Product.jsx
+++ ecommerce/components/Product.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import { urlFor } from '../lib/client'
 
-const Product = ({product:{image,name,slug,price}}) => {
+const Product = ({product:{image,name,slug,price,pais}}) => {
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
@@ -13,6 +13,7 @@ const Product = ({product:{image,name,slug,price}}) => {
             height={250}
             className="product-image"
           />
+          <p className='product-name'>{pais}</p>
           <p className='product-name'>{name}</p>
           <p className='product-price'>${price}</p>
         </div>
